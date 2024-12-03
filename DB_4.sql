ab08d445ce141534f928fb45ace1939adadaea60
parkh12 <parkhunsong12@gmail.com>
2024-12-03 20:16:07 +0900

    Add files via upload

---
+++ DB_4.sql
@@ -0,0 +1,20 @@
+-- 초급 1
+INSERT INTO customers (name, city, phone_number) VALUES ('Bob', 'Seoul', 01012345678);
+-- 초급 2
+INSERT INTO products (productCode, productName) VALUES (123, 'PEN'); 
+-- 초급 3
+INSERT INTO employees ( employeeNumber, lastName, firstName) VALUES (456, 'John','Pa');
+-- 초급 4
+INSERT INTO offices (officeCode,city,phone) VALUES(123, 'SEOUL',01012345678);
+-- 초급 5
+INSERT INTO orders (orderNumber,orderDate) VALUES(123, '12/03');
+-- 초급 6
+INSERT INTO orderdetails (orderNumber,productCode) VALUES (123,123);
+-- 초급 7
+INSERT INTO payments ( customerNumber,checkNumber)  VALUES (123, 213);
+-- 초급 8
+INSERT INTO productlines (productLine) VALUES ('Truck');
+-- 초급 9
+INSERT INTO customers (name, city)  VALUES ('John','Busan');
+-- 초급 10
+INSERT INTO products (productCode,productName,productLine) VALUES (789, 'Bongo', 'Truck');
